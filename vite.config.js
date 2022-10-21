a995bba2435e22c32ae653fb47c944aa423a55be
Neerosh <torgash.wraith@gmail.com>
2022-10-20 21:21:40 -0300

    Added random hash to filenames

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { hash } from './src/utils/functions.js'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
