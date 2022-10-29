c73c2e68ff63d38f99379d0fe9de4313b4e432c1
Neerosh <torgash.wraith@gmail.com>
2022-10-29 17:16:42 -0300

    Removed hash function from vite build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { hash } from './src/utils/functions.js'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name]` + hash + `.js`,
-        chunkFileNames: `[name]` + hash + `.js`,
-        assetFileNames: `[name]` + hash + `.[ext]`
-      }
-    }
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
