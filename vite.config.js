eefd61d1aab7d4e15537369159f47146a1e1bc02
Neerosh <torgash.wraith@gmail.com>
2022-12-02 09:14:55 -0300

    Restored api route

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://neerosh-fullstack.vercel.app/',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,      
         ws: true,
